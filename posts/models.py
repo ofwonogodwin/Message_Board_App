14694bb6d1dd0d793bf8f98c6a500e3e17d03f4d
Godwin Ofwono <godwinofwono933@gmail.com>
2025-05-06 11:45:55 +0300

    Database configurations on the admin side

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+# Data Bases deal with classes
+class post(models.Model):
+    text = models.TextField(default='Input text here')
+
+# post is the new database model
+# text is the data base field and type to hold the textfield
+    def __str__(self):
+        return self.text[:50]
